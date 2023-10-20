2d2cdeba457d778567e9fffb50c549614c9d40e8
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2023-10-20 13:02:14 -0700

    Add windows support

--- minecraft/server.py
+++ minecraft/server.py
@@ -20,13 +20,13 @@ Contact Information:
     - GitHub: https://github.com/megabyte6
 """
 
+import os
+import shutil
+import stat
+import subprocess
+import sys
 from argparse import ArgumentParser
 from datetime import datetime
-from os import chdir, chmod, makedirs, stat
-from os.path import exists, join
-from stat import S_IEXEC
-from subprocess import PIPE, run
-from sys import argv, executable, exit
 
 
 def add_scripts(server_name):
@@ -36,7 +36,7 @@ def add_scripts(server_name):
     :return: None
     """
 
-    if executable.endswith("python3"):
+    if sys.executable.endswith("python3"):
         python_executable = "python3"
     else:
         python_executable = "python"
@@ -44,12 +44,11 @@ def add_scripts(server_name):
     update_script = (
         f"#!/usr/bin/env {python_executable}\n\n"
         + """
+import json
+import os
+import sys
+import urllib.request
 from argparse import ArgumentParser
-from json import load
-from os import listdir, remove
-from os.path import isfile
-from sys import exit
-from urllib.request import urlopen
 
 
 def fetch_json(url: str):
@@ -63,8 +62,8 @@ def fetch_json(url: str):
         The parsed JSON.
     \"""
 
-    with urlopen(url) as response:
-        return load(response)
+    with urllib.request.urlopen(url) as response:
+        return json.load(response)
 
 
 parser = ArgumentParser(description="Update a PaperMC Minecraft server.")
@@ -106,7 +105,7 @@ if args.check_latest:
         print(args.papermc_build)
     else:
         print(f"Latest build for Minecraft {args.mc_version} is version {args.papermc_build}")
-    exit()
+    sys.exit()
 
 
 # Find JAR name for download link.
@@ -116,91 +115,130 @@ jar_name = fetch_json(jar_url)["downloads"]["application"]["name"]
 download_url = f"https://api.papermc.io/v2/projects/paper/versions/{args.mc_version}/builds/{args.papermc_build}/downloads/{jar_name}"
 
 # Check if the latest build is already downloaded.
-if isfile(jar_name):
+if os.path.isfile(jar_name):
     print(f"You are already on the latest build for Minecraft {args.mc_version}")
-    exit()
+    sys.exit()
 
 # Delete old JAR.
-for file in listdir():
+for file in os.listdir():
     if file.startswith("paper") and file.endswith(".jar"):
-        remove(file)
+        os.remove(file)
 
 # Download the latest build of PaperMC.
-with urlopen(download_url) as response, open(jar_name, "wb") as f:
+with urllib.request.urlopen(download_url) as response, open(jar_name, "wb") as f:
     f.write(response.read())
 """.lstrip(
             "\n"
         )
     )
 
-    update_script_path = join(server_name, "update.py")
+    update_script_path = os.path.join(server_name, "update.py")
     with open(update_script_path, "w") as f:
         f.write(update_script)
-    # Make the script executable.
-    chmod(update_script_path, stat(update_script_path).st_mode | S_IEXEC)
+    if not is_windows():
+        # Make the script executable.
+        os.chmod(update_script_path, os.stat(update_script_path).st_mode | stat.S_IEXEC)
 
-    mc_version = (
-        run([join(".", server_name, "update.py"), "--check-latest", "mc-version"], stdout=PIPE).stdout.decode().strip()
-    )
+    if is_windows():
+        mc_version = (
+            subprocess.run(
+                ["py", os.path.join(".", server_name, "update.py"), "--check-latest", "mc-version"],
+                stdout=subprocess.PIPE,
+            )
+            .stdout.decode()
+            .strip()
+        )
+    else:
+        mc_version = (
+            subprocess.run(
+                [os.path.join(".", server_name, "update.py"), "--check-latest", "mc-version"], stdout=subprocess.PIPE
+            )
+            .stdout.decode()
+            .strip()
+        )
     start_script = f"""
 #!/usr/bin/env {python_executable}
 
-from glob import glob
-from os.path import join
-from subprocess import run
+import glob
+import os.path
+import subprocess
 
 # Check if there is an update and if so, update the server JAR.
-run([join(".", "update.py"), "--mc-version", "{mc_version}"])
+subprocess.run([os.path.join(".", "update.py"), "--mc-version", "{mc_version}"])
 
 # Start PaperMC.
-papermc_jar = glob("paper*.jar")[0]
-run(["java", "-Xms512M", "-Xmx4G", "-jar", papermc_jar, "nogui"])
+papermc_jar = glob.glob("paper*.jar")[0]
+subprocess.run(["java", "-Xms512M", "-Xmx4G", "-jar", papermc_jar, "nogui"])
 """.lstrip(
         "\n"
     )
 
-    start_script_path = join(server_name, "start.py")
+    start_script_path = os.path.join(server_name, "start.py")
     with open(start_script_path, "w") as f:
         f.write(start_script)
-    # Make the script executable.
-    chmod(start_script_path, stat(start_script_path).st_mode | S_IEXEC)
+    if not is_windows():
+        # Make the script executable.
+        os.chmod(start_script_path, os.stat(start_script_path).st_mode | stat.S_IEXEC)
 
+
+def is_windows():
+    return "win" in sys.platform
+
+
+def is_macos():
+    return sys.platform == "darwin"
+
+
+def is_linux():
+    return sys.platform == "linux"
+
+
+if not is_windows() and not is_macos() and not is_linux():
+    print("Unsupported operating system.")
+    exit(1)
 
 parser = ArgumentParser(description="Setup or backup a Minecraft server.")
 
 parser.add_argument("-y", action="store_true", help="Answer yes to all prompts")
-parser.add_argument(
-    "-s", "--session", action="store_true", help="Continue or start a Minecraft server's console session"
-)
+if is_linux():
+    parser.add_argument(
+        "-s", "--session", action="store_true", help="Continue or start a Minecraft server's console session"
+    )
 
 server_options = parser.add_mutually_exclusive_group()
 server_options.add_argument("-n", "--new", action="store_true", help="Create a new server")
 server_options.add_argument("-b", "--backup", action="store_true", help="Backup an existing server")
 server_options.add_argument("-d", "--delete", action="store_true", help="Delete an existing server")
 
-parser.add_argument("--list-sessions", action="store_true", help="List all running Minecraft server sessions")
+if is_linux():
+    parser.add_argument("--list-sessions", action="store_true", help="List all running Minecraft server sessions")
 parser.add_argument(
     "server_name", nargs="?", help="The name of the Minecraft server to create or perform the action on"
+)
+parser.add_argument(
+    "--world-name",
+    default="world",
+    help="Specify the world name as is in server.properties. Only needs to be set if the world save name is not the default.",
 )
 
 args = parser.parse_args()
 
 # Print usage example if no arguments are passed.
-if len(argv) == 1:
+if len(sys.argv) == 1:
     parser.print_usage()
     print("run with '-h' to get help")
-    exit()
+    sys.exit()
 
-if args.list_sessions:
-    sessions = run(
+if is_linux() and args.list_sessions:
+    sessions = subprocess.run(
         [
             "tmux",
             "ls",
             "-F",
             "#{session_name}: #{session_windows} windows (created #{t:session_created}) (#{?session_attached,attached,not attached})",
         ],
-        stdout=PIPE,
-        stderr=PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
     )
     # No sessions found would result in an error.
     if sessions.stderr.decode():
@@ -208,90 +246,94 @@ if args.list_sessions:
     else:
         print(sessions.stdout.decode())
 
-    exit()
+    sys.exit()
 
 if args.new:
     # Check if the server given exists.
-    if exists(args.server_name):
+    if os.path.exists(args.server_name):
         print(f"A server with the name '{args.server_name}' already exists.")
-        exit(1)
+        sys.exit(1)
 
     # Create the server directory and add the custom scripts.
-    makedirs(args.server_name, exist_ok=True)
+    os.makedirs(args.server_name, exist_ok=True)
     add_scripts(args.server_name)
 
 elif args.backup:
     # Check if the server given exists.
-    if not exists(args.server_name):
+    if not os.path.exists(args.server_name):
         print(f"A server with the name '{args.server_name}' does not exist.")
         print("Please check the spelling and try again.")
-        exit(1)
+        sys.exit(1)
 
     # Define the backup location.
     if args.server_name[-1] in ["/", "\\"]:
         args.server_name = args.server_name[:-1]
 
     # Navigate to the server directory.
-    chdir(args.server_name)
+    # os.chdir(args.server_name)
 
-    world_directories = ["world", "world_nether", "world_the_end"]
+    world_saves = [
+        os.path.join(args.server_name, args.world_name),
+        os.path.join(args.server_name, f"{args.world_name}_nether"),
+        os.path.join(args.server_name, f"{args.world_name}_the_end"),
+    ]
     # Make sure the world save directories exist.
-    for directory in world_directories:
-        if not exists(directory):
-            print(f"Could not find a world save at '{directory}' in '{args.server_name}'.")
-            exit(1)
+    for directory in world_saves:
+        if not os.path.exists(directory):
+            print(f"Could not find a world save at '{directory}'")  # in '{args.server_name}'.")
+            sys.exit(1)
 
     current_date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    backup_directory = "backup"
-    makedirs(backup_directory, exist_ok=True)
-    backup_path = join(backup_directory, f"{current_date}.tar.xz")
+    backup_directory = os.path.join(args.server_name, "backup")
+    os.makedirs(backup_directory, exist_ok=True)
+    backup_path = os.path.join(backup_directory, f"{current_date}.tar.xz")
 
-    run(["tar", "-cvJf", backup_path, *world_directories])
+    subprocess.run(["tar", "-cvJf", backup_path, *world_saves])
 
     # Navigate back to the original directory.
-    chdir("..")
+    # os.chdir("..")
 
 elif args.delete:
     # Check if the server given exists.
-    if not exists(args.server_name):
+    if not os.path.exists(args.server_name):
         print(f"A server with the name '{args.server_name}' does not exist.")
         print("Please check the spelling and try again.")
-        exit(1)
+        sys.exit(1)
 
     try:
         delete_confirmation = input(
             f"Are you sure you want to delete '{args.server_name}'? This will delete the server backups as well. (y/N): "
         )
     except KeyboardInterrupt:
-        exit()
+        sys.exit()
     if not args.y and delete_confirmation.lower() not in ["y", "yes"]:
-        exit()
+        sys.exit()
 
     # Delete the server directory.
-    run(["rm", "-rf", args.server_name])
+    shutil.rmtree(args.server_name)
 
-    exit()
+    sys.exit()
 
-if args.session:
+if is_linux() and args.session:
     # The tmux id should not contain any slashes.
     if args.server_name[-1] in ["/", "\\"]:
         args.server_name = args.server_name[:-1]
     tmux_id = f"mc-{args.server_name}"
 
     # Check if the server name given exists.
-    if not exists(args.server_name):
+    if not os.path.exists(args.server_name):
         print(f"A server with the name '{args.server_name}' does not exist.")
         if args.server_name.startswith("mc-"):
             print(f"Did you mean './server.py -s {args.server_name[3:]}'?")
-        exit(1)
+        sys.exit(1)
 
     # Check if the server is already running.
-    tmux_sessions = run(["tmux", "ls"], stdout=PIPE, stderr=PIPE)
+    tmux_sessions = subprocess.run(["tmux", "ls"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if tmux_id in tmux_sessions.stdout.decode():
-        run(["tmux", "attach", "-t", tmux_id])
+        subprocess.run(["tmux", "attach", "-t", tmux_id])
     else:
-        chdir(args.server_name)
+        os.chdir(args.server_name)
 
         # Use systemd-run to run tmux as a user process to prevent it from being killed when the user logs out.
         # If this is a new server, run 'loginctl enable-linger' to allow the process to stay active even if all users log off.
-        run(["systemd-run", "--scope", "--user", "tmux", "new", "-s", tmux_id])
+        subprocess.run(["systemd-run", "--scope", "--user", "tmux", "new", "-s", tmux_id])
